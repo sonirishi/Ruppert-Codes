d9c70ca3637ad3fbc063af658edb3321a152cb99
sonirishi <rishabhsoni.iit@gmail.com>
2018-09-17 23:09:53 +0530

    Add files via upload

--- chapter12_regression.R
+++ chapter12_regression.R
@@ -76,3 +76,104 @@ summary(nelson_model2)
 
 extractAIC(nelson_model2)  ## Best way is to loop through all models
 
+########
+source("E:/Documents/Practice/standard_code.R")
+
+nelsonplosser <- read.csv("nelsonplosser.csv")
+
+nelsonplosser <- nelsonplosser %>% select(sp,gnp.r,gnp.pc,ip,cpi,emp,bnd)
+
+nelsonplosser1 <- nelsonplosser[complete.cases(nelsonplosser),]
+
+nelsonplosser1 <- nelsonplosser1 %>% 
+  mutate(target = log(sp) - lag(log(sp)), x1 = gnp.r - lag(gnp.r),
+         x2 = gnp.pc - lag(gnp.pc), x3 = log(ip) - lag(log(ip)), 
+         x4 = log(cpi) - lag(log(cpi)), x5 = emp - lag(emp), x6 = bnd - lag(bnd))
+
+nelsonplosser2 <- nelsonplosser1[complete.cases(nelsonplosser1),]
+
+dat = read.table(file="WeekInt.txt",header=T)
+
+dat <- dat %>% mutate(cm10_diff = cm10 - lag(cm10), aaa_diff = aaa - lag(aaa),
+                      ff_diff = ff - lag(ff), cm30_diff = cm30 - lag(cm30))
+
+dat <- dat[complete.cases(dat),]
+
+lm_model3 <- lm(aaa_diff ~ cm10_diff + cm30_diff, data = dat)
+
+partial_residual_cm10diff <- dat$aaa_diff - lm_model3$coefficients[1] - 
+  lm_model3$coefficients[3]*dat$cm30_diff
+
+partial_residual_cm30diff <- dat$aaa_diff - lm_model3$coefficients[1] - 
+  lm_model3$coefficients[2]*dat$cm10_diff
+
+plot(x = dat$cm10_diff, y = partial_residual_cm10diff)
+
+plot(x=lm_model3$fitted.values, y = lm_model3$residuals, col = 'green')
+acf(lm_model3$fitted.values)
+
+######
+library(AER)
+
+data("USMacroG")
+
+Macrodiff <- apply(USMacroG,2,diff)
+
+pairs(cbind(Macrodiff[,2],Macrodiff[,5],Macrodiff[,4],Macrodiff[,6],Macrodiff[,9]))
+
+model <- lm(Macrodiff[,2] ~ Macrodiff[,5]+Macrodiff[,4]+Macrodiff[,6]+Macrodiff[,9])
+
+summary(model)
+
+confint(model)
+
+###############
+
+anova(model)
+
+library(MASS)
+
+model_2 <- stepAIC(model)
+summary(model_2)
+
+AIC(model)
+
+AIC(model_2)  ## not much change in AIC
+
+library(car)
+
+vif(model)
+
+vif(model_2)
+
+par(mfrow=c(2,2))
+sp=0.8
+crPlot(model,Macrodiff[,5])
+
+#############
+
+X <- rnorm(100000,1,0.7)
+res <- rnorm(100000,0,0.3)
+
+y <- 1.4 + 1.7*X + res
+
+plot(density(y))
+
+y <- X + res
+
+plot(density(y))
+
+##########
+
+x <- seq(1,15,0.5)
+
+x2 <- x^2
+
+cor(x,x2)
+
+x_scale <- x - mean(x)
+x2_scale <- x2 - mean(x2)
+
+cor(x_scale,x2_scale)
+
+
